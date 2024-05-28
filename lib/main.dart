a864d7446abbe0031d7ee2c66df9bd05cf50387b
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-05-28 18:28:50 +0100

    chore: Ignore .env file and add .env.example for Supabase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_quickstart/pages/account_page.dart';
 import 'package:supabase_quickstart/pages/login_page.dart';
 import 'package:supabase_quickstart/pages/splash_page.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   await Supabase.initialize(
-    url: 'https://yojnxscjecnlltwblvrn.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlvam54c2NqZWNubGx0d2JsdnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4ODg1NDUsImV4cCI6MjAzMjQ2NDU0NX0.zH1CZLJPZ-HLO768Vis_8VFgDMmKbpkglVBCYHUcgP0',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+  
   runApp(const MyApp());
 }
 
